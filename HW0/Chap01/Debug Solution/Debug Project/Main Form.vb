2e9e28d3dac4f2f39a823593beffa131f215b01b
Ian Malerich <ian@malerich.net>
2018-01-14 00:39:07 -0800

    Homework 0.

---
+++ HW0/Chap01/Debug Solution/Debug Project/Main Form.vb
@@ -0,0 +1,8 @@
+Public Class MainForm
+    ' Ian Malerich
+    ' imm@iastate.edu
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Me.Close()
+    End Sub
+End Class
