3ad2f3f7a8543402e4956495d560645870428b05
Ian Malerich <ian@malerich.net>
2018-03-31 03:50:59 -0500

    Finished Assignment 8.

--- Car Shoppers Solution/Car Shoppers Project/Main Form.Designer.vb
+++ Car Shoppers Solution/Car Shoppers Project/Main Form.Designer.vb
@@ -19,224 +19,224 @@ Partial Public Class MainForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.bankPayLabel = New System.Windows.Forms.Label()
-        Me.Label7 = New System.Windows.Forms.Label()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.termListBox = New System.Windows.Forms.ListBox()
-        Me.bankRateListBox = New System.Windows.Forms.ListBox()
-        Me.dealerRateListBox = New System.Windows.Forms.ListBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.rebateTextBox = New System.Windows.Forms.TextBox()
-        Me.priceTextBox = New System.Windows.Forms.TextBox()
-        Me.dealerPayLabel = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
-        Me.exitButton = New System.Windows.Forms.Button()
-        Me.calcButton = New System.Windows.Forms.Button()
-        Me.GroupBox1.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'bankPayLabel
-        '
-        Me.bankPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.bankPayLabel.Location = New System.Drawing.Point(306, 204)
-        Me.bankPayLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.bankPayLabel.Name = "bankPayLabel"
-        Me.bankPayLabel.Size = New System.Drawing.Size(150, 25)
-        Me.bankPayLabel.TabIndex = 6
-        Me.bankPayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label7
-        '
-        Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(303, 183)
-        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(142, 17)
-        Me.Label7.TabIndex = 5
-        Me.Label7.Text = "Bank monthly payment:"
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.Controls.Add(Me.termListBox)
-        Me.GroupBox1.Controls.Add(Me.bankRateListBox)
-        Me.GroupBox1.Controls.Add(Me.dealerRateListBox)
-        Me.GroupBox1.Controls.Add(Me.Label5)
-        Me.GroupBox1.Controls.Add(Me.Label4)
-        Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Controls.Add(Me.Label2)
-        Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Controls.Add(Me.rebateTextBox)
-        Me.GroupBox1.Controls.Add(Me.priceTextBox)
-        Me.GroupBox1.Location = New System.Drawing.Point(21, 11)
-        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
-        Me.GroupBox1.Size = New System.Drawing.Size(550, 145)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
-        '
-        'termListBox
-        '
-        Me.termListBox.FormattingEnabled = True
-        Me.termListBox.ItemHeight = 17
-        Me.termListBox.Location = New System.Drawing.Point(433, 50)
-        Me.termListBox.Margin = New System.Windows.Forms.Padding(2)
-        Me.termListBox.Name = "termListBox"
-        Me.termListBox.Size = New System.Drawing.Size(55, 72)
-        Me.termListBox.TabIndex = 9
-        '
-        'bankRateListBox
-        '
-        Me.bankRateListBox.FormattingEnabled = True
-        Me.bankRateListBox.ItemHeight = 17
-        Me.bankRateListBox.Location = New System.Drawing.Point(347, 50)
-        Me.bankRateListBox.Margin = New System.Windows.Forms.Padding(2)
-        Me.bankRateListBox.Name = "bankRateListBox"
-        Me.bankRateListBox.Size = New System.Drawing.Size(70, 72)
-        Me.bankRateListBox.TabIndex = 7
-        '
-        'dealerRateListBox
-        '
-        Me.dealerRateListBox.FormattingEnabled = True
-        Me.dealerRateListBox.ItemHeight = 17
-        Me.dealerRateListBox.Location = New System.Drawing.Point(257, 50)
-        Me.dealerRateListBox.Margin = New System.Windows.Forms.Padding(2)
-        Me.dealerRateListBox.Name = "dealerRateListBox"
-        Me.dealerRateListBox.Size = New System.Drawing.Size(70, 72)
-        Me.dealerRateListBox.TabIndex = 5
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(430, 31)
-        Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(84, 17)
-        Me.Label5.TabIndex = 8
-        Me.Label5.Text = "&Term (years):"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(344, 31)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(65, 17)
-        Me.Label4.TabIndex = 6
-        Me.Label4.Text = "&Bank rate:"
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(8, 31)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 17)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "&Price:"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(131, 31)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(52, 17)
-        Me.Label2.TabIndex = 2
-        Me.Label2.Text = "&Rebate:"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(254, 31)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(76, 17)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "&Dealer rate:"
-        '
-        'rebateTextBox
-        '
-        Me.rebateTextBox.Location = New System.Drawing.Point(134, 50)
-        Me.rebateTextBox.Margin = New System.Windows.Forms.Padding(2)
-        Me.rebateTextBox.Name = "rebateTextBox"
-        Me.rebateTextBox.Size = New System.Drawing.Size(90, 25)
-        Me.rebateTextBox.TabIndex = 3
-        '
-        'priceTextBox
-        '
-        Me.priceTextBox.Location = New System.Drawing.Point(11, 50)
-        Me.priceTextBox.Margin = New System.Windows.Forms.Padding(2)
-        Me.priceTextBox.Name = "priceTextBox"
-        Me.priceTextBox.Size = New System.Drawing.Size(90, 25)
-        Me.priceTextBox.TabIndex = 1
-        '
-        'dealerPayLabel
-        '
-        Me.dealerPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.dealerPayLabel.Location = New System.Drawing.Point(136, 204)
-        Me.dealerPayLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.dealerPayLabel.Name = "dealerPayLabel"
-        Me.dealerPayLabel.Size = New System.Drawing.Size(150, 25)
-        Me.dealerPayLabel.TabIndex = 4
-        Me.dealerPayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(133, 183)
-        Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(153, 17)
-        Me.Label6.TabIndex = 3
-        Me.Label6.Text = "Dealer monthly payment:"
-        '
-        'exitButton
-        '
-        Me.exitButton.Location = New System.Drawing.Point(368, 252)
-        Me.exitButton.Margin = New System.Windows.Forms.Padding(2)
-        Me.exitButton.Name = "exitButton"
-        Me.exitButton.Size = New System.Drawing.Size(88, 32)
-        Me.exitButton.TabIndex = 2
-        Me.exitButton.Text = "E&xit"
-        '
-        'calcButton
-        '
-        Me.calcButton.Location = New System.Drawing.Point(136, 252)
-        Me.calcButton.Margin = New System.Windows.Forms.Padding(2)
-        Me.calcButton.Name = "calcButton"
-        Me.calcButton.Size = New System.Drawing.Size(186, 32)
-        Me.calcButton.TabIndex = 1
-        Me.calcButton.Text = "&Calculate Monthly Payments"
-        '
-        'MainForm
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(593, 307)
-        Me.Controls.Add(Me.bankPayLabel)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.dealerPayLabel)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.exitButton)
-        Me.Controls.Add(Me.calcButton)
-        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.MaximizeBox = False
-        Me.Name = "MainForm"
-        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Car Shoppers Inc."
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
+		Me.bankPayLabel = New System.Windows.Forms.Label()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.termListBox = New System.Windows.Forms.ListBox()
+		Me.bankRateListBox = New System.Windows.Forms.ListBox()
+		Me.dealerRateListBox = New System.Windows.Forms.ListBox()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.rebateTextBox = New System.Windows.Forms.TextBox()
+		Me.priceTextBox = New System.Windows.Forms.TextBox()
+		Me.dealerPayLabel = New System.Windows.Forms.Label()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.exitButton = New System.Windows.Forms.Button()
+		Me.calcButton = New System.Windows.Forms.Button()
+		Me.GroupBox1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'bankPayLabel
+		'
+		Me.bankPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.bankPayLabel.Location = New System.Drawing.Point(306, 204)
+		Me.bankPayLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.bankPayLabel.Name = "bankPayLabel"
+		Me.bankPayLabel.Size = New System.Drawing.Size(150, 25)
+		Me.bankPayLabel.TabIndex = 6
+		Me.bankPayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Location = New System.Drawing.Point(303, 183)
+		Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(142, 17)
+		Me.Label7.TabIndex = 5
+		Me.Label7.Text = "Bank monthly payment:"
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.Controls.Add(Me.termListBox)
+		Me.GroupBox1.Controls.Add(Me.bankRateListBox)
+		Me.GroupBox1.Controls.Add(Me.dealerRateListBox)
+		Me.GroupBox1.Controls.Add(Me.Label5)
+		Me.GroupBox1.Controls.Add(Me.Label4)
+		Me.GroupBox1.Controls.Add(Me.Label1)
+		Me.GroupBox1.Controls.Add(Me.Label2)
+		Me.GroupBox1.Controls.Add(Me.Label3)
+		Me.GroupBox1.Controls.Add(Me.rebateTextBox)
+		Me.GroupBox1.Controls.Add(Me.priceTextBox)
+		Me.GroupBox1.Location = New System.Drawing.Point(21, 11)
+		Me.GroupBox1.Margin = New System.Windows.Forms.Padding(2)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Padding = New System.Windows.Forms.Padding(2)
+		Me.GroupBox1.Size = New System.Drawing.Size(550, 145)
+		Me.GroupBox1.TabIndex = 0
+		Me.GroupBox1.TabStop = False
+		'
+		'termListBox
+		'
+		Me.termListBox.FormattingEnabled = True
+		Me.termListBox.ItemHeight = 17
+		Me.termListBox.Location = New System.Drawing.Point(433, 50)
+		Me.termListBox.Margin = New System.Windows.Forms.Padding(2)
+		Me.termListBox.Name = "termListBox"
+		Me.termListBox.Size = New System.Drawing.Size(55, 72)
+		Me.termListBox.TabIndex = 9
+		'
+		'bankRateListBox
+		'
+		Me.bankRateListBox.FormattingEnabled = True
+		Me.bankRateListBox.ItemHeight = 17
+		Me.bankRateListBox.Location = New System.Drawing.Point(347, 50)
+		Me.bankRateListBox.Margin = New System.Windows.Forms.Padding(2)
+		Me.bankRateListBox.Name = "bankRateListBox"
+		Me.bankRateListBox.Size = New System.Drawing.Size(70, 72)
+		Me.bankRateListBox.TabIndex = 7
+		'
+		'dealerRateListBox
+		'
+		Me.dealerRateListBox.FormattingEnabled = True
+		Me.dealerRateListBox.ItemHeight = 17
+		Me.dealerRateListBox.Location = New System.Drawing.Point(257, 50)
+		Me.dealerRateListBox.Margin = New System.Windows.Forms.Padding(2)
+		Me.dealerRateListBox.Name = "dealerRateListBox"
+		Me.dealerRateListBox.Size = New System.Drawing.Size(70, 72)
+		Me.dealerRateListBox.TabIndex = 5
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.Location = New System.Drawing.Point(430, 31)
+		Me.Label5.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(83, 17)
+		Me.Label5.TabIndex = 8
+		Me.Label5.Text = "&Term (years):"
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Location = New System.Drawing.Point(344, 31)
+		Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(65, 17)
+		Me.Label4.TabIndex = 6
+		Me.Label4.Text = "&Bank rate:"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(8, 31)
+		Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(39, 17)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "&Price:"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(131, 31)
+		Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(52, 17)
+		Me.Label2.TabIndex = 2
+		Me.Label2.Text = "&Rebate:"
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(254, 31)
+		Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(76, 17)
+		Me.Label3.TabIndex = 4
+		Me.Label3.Text = "&Dealer rate:"
+		'
+		'rebateTextBox
+		'
+		Me.rebateTextBox.Location = New System.Drawing.Point(134, 50)
+		Me.rebateTextBox.Margin = New System.Windows.Forms.Padding(2)
+		Me.rebateTextBox.Name = "rebateTextBox"
+		Me.rebateTextBox.Size = New System.Drawing.Size(90, 25)
+		Me.rebateTextBox.TabIndex = 3
+		'
+		'priceTextBox
+		'
+		Me.priceTextBox.Location = New System.Drawing.Point(11, 50)
+		Me.priceTextBox.Margin = New System.Windows.Forms.Padding(2)
+		Me.priceTextBox.Name = "priceTextBox"
+		Me.priceTextBox.Size = New System.Drawing.Size(90, 25)
+		Me.priceTextBox.TabIndex = 1
+		'
+		'dealerPayLabel
+		'
+		Me.dealerPayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+		Me.dealerPayLabel.Location = New System.Drawing.Point(136, 204)
+		Me.dealerPayLabel.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.dealerPayLabel.Name = "dealerPayLabel"
+		Me.dealerPayLabel.Size = New System.Drawing.Size(150, 25)
+		Me.dealerPayLabel.TabIndex = 4
+		Me.dealerPayLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Label6
+		'
+		Me.Label6.AutoSize = True
+		Me.Label6.Location = New System.Drawing.Point(133, 183)
+		Me.Label6.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(153, 17)
+		Me.Label6.TabIndex = 3
+		Me.Label6.Text = "Dealer monthly payment:"
+		'
+		'exitButton
+		'
+		Me.exitButton.Location = New System.Drawing.Point(368, 252)
+		Me.exitButton.Margin = New System.Windows.Forms.Padding(2)
+		Me.exitButton.Name = "exitButton"
+		Me.exitButton.Size = New System.Drawing.Size(88, 32)
+		Me.exitButton.TabIndex = 2
+		Me.exitButton.Text = "E&xit"
+		'
+		'calcButton
+		'
+		Me.calcButton.Location = New System.Drawing.Point(136, 252)
+		Me.calcButton.Margin = New System.Windows.Forms.Padding(2)
+		Me.calcButton.Name = "calcButton"
+		Me.calcButton.Size = New System.Drawing.Size(186, 32)
+		Me.calcButton.TabIndex = 1
+		Me.calcButton.Text = "&Calculate Monthly Payments"
+		'
+		'MainForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(593, 307)
+		Me.Controls.Add(Me.bankPayLabel)
+		Me.Controls.Add(Me.Label7)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Controls.Add(Me.dealerPayLabel)
+		Me.Controls.Add(Me.Label6)
+		Me.Controls.Add(Me.exitButton)
+		Me.Controls.Add(Me.calcButton)
+		Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.MaximizeBox = False
+		Me.Name = "MainForm"
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+		Me.Text = "Car Shoppers Inc."
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
 
-    End Sub
-    Friend WithEvents bankPayLabel As System.Windows.Forms.Label
+	End Sub
+	Friend WithEvents bankPayLabel As System.Windows.Forms.Label
     Friend WithEvents Label7 As System.Windows.Forms.Label
     Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
     Friend WithEvents Label5 As System.Windows.Forms.Label
--- Car Shoppers Solution/Car Shoppers Project/Main Form.vb
+++ Car Shoppers Solution/Car Shoppers Project/Main Form.vb
@@ -1,196 +1,96 @@
 ' Project name:         Car Shoppers Project
 ' Project purpose:      Displays the monthly payments on a loan
-' Your Name :
+' Your Name :			Ian Malerich (imm@iastate.edu)
 
 Option Explicit On
 Option Strict On
 Option Infer Off
 
-
-
-
 Public Class MainForm
 
-    'Function to calculates a monthly payment on a loan
+	'Function to calculates a monthly payment on a loan
+	'1. Complete the CalcPayment function header
+	Private Function CalcPayment(ByVal rate As Double, ByVal years As Double, ByVal principal As Double) As Double
+		Dim payment As Double
+		'Calculate the payment using Financial.Pmt function.
+		payment = -Financial.Pmt(rate / 12, years * 12, principal)
+		Return payment
+	End Function
 
-    '1. Complete the CalcPayment function header
+	' Sub procedure to fills the list boxes with values, then selects the first item
+	Private Sub MainForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+		For rate As Double = 0.02 To 0.15 Step 0.01
+			dealerRateListBox.Items.Add(rate.ToString)
+			bankRateListBox.Items.Add(rate.ToString)
+		Next rate
 
-    Private _ _ _ _ CalcPayment(ByVal rate As _ _ _ _, ByVal years As _ _ _ _, ByVal principal As _ _ _ _) As _ _ _ _
+		'2.	Write code to fill the termListBox with values from 2 to 5 (Integer type) within the following space
+		For year As Integer = 2 To 5
+			termListBox.Items.Add(year.ToString)
+		Next
 
+		dealerRateListBox.SelectedIndex = 0
+		bankRateListBox.SelectedIndex = 0
+		termListBox.SelectedIndex = 0
+	End Sub
 
-    Dim payment As Double
-
-    'Calculate the payment using Financial.Pmt function.
-        payment = -Financial.Pmt(rate / 12, years * 12, principal)
-
-        Return payment
-
-    End Function
-
-
-
-
-
-
-
-
-
-
-    '-------------------------------------------------------------------------------------------------------------------------------------------------
-
-    ' Sub procedure to fills the list boxes with values, then selects the first item
-
-    Private Sub MainForm_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
-
-        For rate As Double = 0.02 To 0.15 Step 0.01
-            dealerRateListBox.Items.Add(rate.ToString)
-            bankRateListBox.Items.Add(rate.ToString)
-        Next rate
-
-        '2.	Write code to fill the termListBox with values from 2 to 5 (Integer type) within the following space
-
-
-
-
-
-
-
-        dealerRateListBox.SelectedIndex = 0
-        bankRateListBox.SelectedIndex = 0
-        termListBox.SelectedIndex = 0
-    End Sub
-
-
-
-
-
-
-
-
-
-    '-------------------------------------------------------------------------------------------------------------------------------------------------
-
-    ' Sub procedure to clear the content of the labels
-
-    Private Sub ClearPayments(ByVal sender As Object, ByVal e As System.EventArgs
+	' Sub procedure to clear the content of the labels
+	Private Sub ClearPayments(ByVal sender As Object, ByVal e As System.EventArgs
         ) Handles priceTextBox.TextChanged, rebateTextBox.TextChanged,
         dealerRateListBox.SelectedValueChanged, bankRateListBox.SelectedValueChanged,
         termListBox.SelectedValueChanged
 
+		'3.	Write code to clear the content of dealerPayLabel and bankPayLabel within the following space
+		dealerPayLabel.Text = ""
+		bankPayLabel.Text = ""
 
-        '3.	Write code to clear the content of dealerPayLabel and bankPayLabel within the following space
+	End Sub
 
+	' Sub procedure to define calcButton click event
+	Private Sub calcButton_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles calcButton.Click
 
-
-
-
-
-
-    End Sub
-
-
-
-
-
-
-
-
-    '-------------------------------------------------------------------------------------------------------------------------------------------------
-
-    ' Sub procedure to define calcButton click event
-
-    Private Sub calcButton_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles calcButton.Click
-
-
-        'declare variables
-        Dim price As Double
+		'declare variables
+		Dim price As Double
         Dim rebate As Double
         Dim dealerRate As Double
         Dim bankRate As Double
         Dim term As Double
         Dim dealerPayment As Double
-        Dim bankPayment As Double
+		Dim bankPayment As Double
 
+		'4.	Write code to get the values at priceTextBox, rebateTextBox to the variables price, rebate respectively within the following space
+		Double.TryParse(priceTextBox.Text, price)
+		Double.TryParse(rebateTextBox.Text, rebate)
 
+		'5.	Write code to get the selected values at dealerRateListBox, bankRateListBox and termListBox to the variables dealerRate, bankRate and term respectively within the following space
+		Double.TryParse(CType(dealerRateListBox.SelectedItem, String), dealerRate)
+		Double.TryParse(CType(bankRateListBox.SelectedItem, String), bankRate)
+		Double.TryParse(CType(termListBox.SelectedItem, String), term)
 
-        '4.	Write code to get the values at priceTextBox, rebateTextBox to the variables price, rebate respectively within the following space
+		'6.	Invoke the function CalcPayment to get the value to the variable dealerPayment by passing the values dealerRate, term and price within the following space
+		dealerPayment = CalcPayment(dealerRate, term, price)
 
+		'7.	Invoke the function CalcPayment to get the value to the variable bankPayment by passing the values bankRate, term and (price – rebate) within the following space
+		bankPayment = CalcPayment(bankRate, term, (price - rebate))
 
-
-
-
-
-
-        '5.	Write code to get the selected values at dealerRateListBox, bankRateListBox and termListBox to the variables dealerRate, bankRate and term respectively within the following space
-
-
-
-
-
-
-
-        '6.	Invoke the function CalcPayment to get the value to the variable dealerPayment by passing the values dealerRate, term and price within the following space
-
-
-
-
-
-
-        '7.	Invoke the function CalcPayment to get the value to the variable bankPayment by passing the values bankRate, term and (price – rebate) within the following space
-
-
-        
-
-
-        dealerPayLabel.Text = dealerPayment.ToString("C2")
+		dealerPayLabel.Text = dealerPayment.ToString("C2")
         bankPayLabel.Text = bankPayment.ToString("C2")
 
     End Sub
 
-
-
-
-
-
-
-
-    '-------------------------------------------------------------------------------------------------------------------------------------------------
-
-    ' Sub procedure to define exitButton click event
-
-    Private Sub exitButton_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles exitButton.Click
+	' Sub procedure to define exitButton click event
+	Private Sub exitButton_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles exitButton.Click
         'ends the application
         Me.Close()
     End Sub
 
+	' Sub procedure to select all the content of the priceTextBox
+	Private Sub priceTextBox_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles priceTextBox.Enter
+		priceTextBox.SelectAll()
+	End Sub
 
-
-
-
-
-
-
-    '-------------------------------------------------------------------------------------------------------------------------------------------------
-
-    ' Sub procedure to select all the content of the priceTextBox
-
-    Private Sub priceTextBox_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles priceTextBox.Enter
-        priceTextBox.SelectAll()
-    End Sub
-
-
-
-
-
-
-
-
-    '-------------------------------------------------------------------------------------------------------------------------------------------------
-
-    ' Sub procedure to select all the content of the rebateTextBox
-
-    Private Sub rebateTextBox_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles rebateTextBox.Enter
+	' Sub procedure to select all the content of the rebateTextBox
+	Private Sub rebateTextBox_Enter(ByVal sender As Object, ByVal e As System.EventArgs) Handles rebateTextBox.Enter
         rebateTextBox.SelectAll()
     End Sub
 
